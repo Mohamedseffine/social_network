d828c62dec36ab4de0453d053fd819303fa4ded8
midbenke <idmossab@gmail.com>
2025-07-31 14:46:29 +0100

    edit profile post

--- backend/internal/domain/models/profil.go
+++ backend/internal/domain/models/profil.go
@@ -1,24 +1,23 @@
 package models
 
 type FullProfileResponse struct {
-	User           UserProfileDTO `json:"user"`
-	FollowersCount int            `json:"followers_count"`
-	FollowingCount int            `json:"following_count"`
-	Posts          []PostWithComments 
+	User           UserProfileDTO     `json:"user"`
+	FollowersCount int                `json:"followers_count"`
+	FollowingCount int                `json:"following_count"`
+	Posts          []PostWithComments `json:"posts"`
 }
 
 type AnotherProfileResponse struct {
-    Id            int     `json:"id"`
-    UserName      string  `json:"username"`
-    FirstName     string  `json:"firstName,omitempty"`   // hide if private
-    LastName      string  `json:"lastName,omitempty"`
-    AvatarUrl     *string `json:"avatarUrl,omitempty"`
-    AboutMe       *string `json:"aboutMe,omitempty"`
-    PrivacyStatus string  `json:"privacyStatus"`
-    FollowersCount int    `json:"followers_count"`
-    FollowingCount int    `json:"following_count"`
-    Posts         []Post  `json:"posts,omitempty"`       // empty or omitted if not allowed
-    IsSelf        bool    `json:"is_self"`
-    IsFollowing   bool    `json:"is_following"`
+	Id             int     `json:"id"`
+	UserName       string  `json:"username"`
+	FirstName      string  `json:"firstName,omitempty"` // hide if private
+	LastName       string  `json:"lastName,omitempty"`
+	AvatarUrl      *string `json:"avatarUrl,omitempty"`
+	AboutMe        *string `json:"aboutMe,omitempty"`
+	PrivacyStatus  string  `json:"privacyStatus"`
+	FollowersCount int     `json:"followers_count"`
+	FollowingCount int     `json:"following_count"`
+	Posts          []Post  `json:"posts,omitempty"` // empty or omitted if not allowed
+	IsSelf         bool    `json:"is_self"`
+	IsFollowing    bool    `json:"is_following"`
 }
-
