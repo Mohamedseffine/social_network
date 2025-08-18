da474ed6ba00e07e814c3df1148f1dd106418d7e
hvtims <timo.goog03@gmail.com>
2025-08-18 05:54:05 +0100

    posts fix  group

--- frontend/src/app/groups/[id]/page.tsx
+++ frontend/src/app/groups/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import "../../style.css"
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { API_BASE_URL, getImageUrl } from "../../../utils/api";
