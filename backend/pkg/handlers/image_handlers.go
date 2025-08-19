83e85a2db95d627a22b8e221e59d419bf1b5e91e
Mohamedseffine <seffinemohamed470@gmail.com>
2025-08-19 12:16:34 +0100

    stil working on the damn images

--- backend/pkg/handlers/image_handlers.go
+++ backend/pkg/handlers/image_handlers.go
@@ -58,18 +58,53 @@ func (app *App) GetImageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	stm, err := app.DB.Prepare(`SELECT COUNT (id) FROM posts WHERE image = ? `)
+	stm4, err := app.DB.Prepare(`SELECT COUNT (id) FROM users WHERE avatar = ? `)
 	if err != nil {
 		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
+		return
+	}
+	err = stm4.QueryRow(imagePath).Scan(&count)
+	if err != nil {
+		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
+		return
+	}
+	if count > 0 {
+		http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads")))
+		return
+	}
+
+	stm, err := app.DB.Prepare(`SELECT COUNT (id) FROM posts WHERE image = ? `)
+	if err != nil {
+		http.Error(w, "error:"+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	err = stm.QueryRow(imagePath).Scan(&count)
 	if err != nil {
-		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
+		http.Error(w, "error:"+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if count>0 {
-		
+	if count > 0 {
+		var status int
+		var pPrivacy string
+		stM, err := app.DB.Prepare(`
+		SELECT u.profile_is_public , p.privacy
+		FROM posts p 
+		INNER JOIN users u ON p.user_id = u.id 
+		WHERE p.image = ?
+		`)
+		if err != nil {
+			http.Error(w, "error:"+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = stM.QueryRow(imagePath).Scan(&status, &pPrivacy)
+		if err != nil {
+			http.Error(w, "error:"+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if status == 1 && pPrivacy == "public" {
+			http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads")))
+			return
+		}
 		return
 	}
 	stm1, err := app.DB.Prepare(`SELECT COUNT (id) FROM comments WHERE image_url = ? `)
@@ -82,8 +117,8 @@ func (app *App) GetImageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
 		return
 	}
-	if count>0 {
-		
+	if count > 0 {
+
 		return
 	}
 	stm2, err := app.DB.Prepare(`SELECT COUNT (id) FROM group_post_comments WHERE image_url = ? `)
@@ -96,8 +131,8 @@ func (app *App) GetImageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
 		return
 	}
-	if count>0 {
-		
+	if count > 0 {
+
 		return
 	}
 	stm3, err := app.DB.Prepare(`SELECT COUNT (id) FROM group_posts WHERE image = ? `)
@@ -110,8 +145,13 @@ func (app *App) GetImageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Image Doesn't Exist", http.StatusNotFound)
 		return
 	}
-	if count>0 {
-		
+	if count > 0 {
+
 		return
 	}
+}
+
+
+func CanSeePost(id int, ){
+	
 }
